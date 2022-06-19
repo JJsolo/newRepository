174d0cea7e6503ac4fb81be6d178150511940013
Juan J Solorzano <juanjose.solorzano.c@hotmail.com>
2022-06-18 20:21:51 -0500

    first commit

---
+++ moving.py
@@ -0,0 +1,17 @@
+from matplotlib.pyplot as plt
+
+class firstclass firstclass:
+    
+    def __init__(self):
+
+        self._definition = "how to know the new options"
+        self._number = 35
+        self.allperson = []
+        self.name = ""
+
+
+    def name(sef, name):
+
+        self.name = name
+        return self.name
+
